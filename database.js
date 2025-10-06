00666a307fe2df2b0115bcbfba3a4454a07baece
qmadfia <qm.adfia@gmail.com>
2025-10-06 14:22:58 +0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,23 @@
+/**
+ * @file database.js
+ * @description Contains the mapping of style numbers to model names.
+ */
+
+const styleModelMap = {
+    'STY001': 'Air Max 270',
+    'STY002': 'Classic Cortez',
+    'STY003': 'React Element 55',
+    'STY004': 'VaporMax Flyknit',
+    'STY005': 'Zoom Pegasus 38',
+    'SEP001': 'Sepatu Lari Cepat',
+    'SEP002': 'Sepatu Basket Pro',
+    'SEK001': 'Sekuter Listrik Urban',
+    'SPD001': 'Sepeda Gunung Trail',
+    'TSH001': 'T-Shirt Katun Premium',
+    'JCK001': 'Jaket Bomber Tahan Air'
+};
+
+const defectTypes = [
+    'Hairy', 'Damage', 'Stain', 'Cracked', 'Wrinkle',
+    'Over Cement', 'Bonding Gap', 'Uneven Color', 'Stitching Issue', 'Component Missing'
+];
